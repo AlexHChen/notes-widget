8a36a6b7fbf84907af63c8b7f679461b5e6cec11
Alex Chen <40147717+AlexHChen@users.noreply.github.com>
2021-02-01 21:56:53 -0500

    deploy react app to github pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAX5kxQorzVSRPjmYjJXF2rjfkKYlvahDk",
+  authDomain: "newtabproject-63546.firebaseapp.com",
+  projectId: "newtabproject-63546",
+  storageBucket: "newtabproject-63546.appspot.com",
+  messagingSenderId: "286756192420",
+  appId: "1:286756192420:web:98446f78da53c196c52631"
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
